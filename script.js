ef9f69aa55d3336bc87001dc25b87f6301217995
Kelly Kim <kellykim408@gmail.com>
2020-09-28 22:15:27 -0700

    create for loop for data list

--- script.js
+++ script.js
@@ -57,5 +57,40 @@ $(document).ready(function() {
             }
         });
      }
+
+     function Forecast(searchCity) {
+        $.ajax({
+          type: "GET",
+          url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=3af88076e37153670ec76a89f5ecc44f&units=imperial",
+          dataType: "json",
+          success: function(data) {
+            // overwrite any existing content with title and empty row
+            $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+    
+            // loop over all forecasts (by 3-hour increments)
+            for (var i = 0; i < data.list.length; i++) {
+              // only look at forecasts around 3:00pm
+              if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                // create html elements for a bootstrap card
+                var col = $("<div>").addClass("col-md-2");
+                var card = $("<div>").addClass("card bg-primary text-white");
+                var body = $("<div>").addClass("card-body p-2");
+    
+                var title = $("<h5>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+    
+                var photo = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+    
+                var p1 = $("<p>").addClass("card-text").text("Temp: " + data.list[i].main.temp_max + " °F");
+                var p2 = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+    
+                // merge together and put on page
+                col.append(card.append(body.append(title, photo, p1, p2)));
+                $("#forecast .row").append(col);
+              }
+            }
+          }
+        });
+      }
+
    
   });
